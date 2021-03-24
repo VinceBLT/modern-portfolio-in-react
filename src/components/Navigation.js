ed20ba630111458e7a8ae8dd0caa4b5f593950a8
Vincent <vincent.bezault@icloud.com>
2021-03-24 21:45:36 +0100

    fix navigation links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,13 +14,13 @@ const Navigation = () => {
             <NavLink to="/project-1" activeClassName="nav-active">
               <li>projet 1</li>
             </NavLink>
-            <NavLink to="/project-1" activeClassName="nav-active">
+            <NavLink to="/project-2" activeClassName="nav-active">
               <li>projet 2</li>
             </NavLink>
-            <NavLink to="/project-1" activeClassName="nav-active">
+            <NavLink to="/project-3" activeClassName="nav-active">
               <li>projet 3</li>
             </NavLink>
-            <NavLink to="/project-1" activeClassName="nav-active">
+            <NavLink to="/project-4" activeClassName="nav-active">
               <li>projet 4</li>
             </NavLink>
           </ul>
