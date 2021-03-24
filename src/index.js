eb42a9eec14a9b7ad5f22e7389aefe136c2d011c
Vincent <vincent.bezault@icloud.com>
2021-03-24 14:00:32 +0100

    react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./style/index.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
